bb436f1d790787629fb8220f61a1af6609e30e78
Michael Ben Yosef <mishico5@gmail.com>
2020-12-19 12:16:34 +0200

    Init commit

---
+++ Exceptions/InvalidFENBoardException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessSharp.Exceptions
+{
+    public class InvalidFENBoardException : Exception
+    {
+        public InvalidFENBoardException() { }
+        public InvalidFENBoardException(string message) : base(message) { }
+        public InvalidFENBoardException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
