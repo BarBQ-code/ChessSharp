0c8b02fa57296542fa73c53f4f39c98448922c3e
Michael Ben Yosef <mishico5@gmail.com>
2020-12-25 14:09:26 +0200

    Init commit

---
+++ Exceptions/InvalidBoardException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessSharp.Exceptions
+{
+    public class InvalidBoardException : Exception
+    {
+        public InvalidBoardException() { }
+
+        public InvalidBoardException(string message) : base(message) { }
+
+        public InvalidBoardException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
