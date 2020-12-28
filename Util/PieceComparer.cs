7124d1fdb9a6c199e179fadc6d38622050e74508
Michael Ben Yosef <mishico5@gmail.com>
2020-12-28 14:00:52 +0200

    Init commit

---
+++ Util/PieceComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ChessSharp
+{
+    public class PieceComparer : IEqualityComparer<Piece>
+    {
+        public bool Equals([AllowNull] Piece x, [AllowNull] Piece y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode([DisallowNull] Piece obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
