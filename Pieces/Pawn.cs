1bc4b93f975dd99606a3fa65f212c560d6264488
Michael Ben Yosef <mishico5@gmail.com>
2021-01-03 16:58:29 +0200

    Documenting Pawn

--- Pieces/Pawn.cs
+++ Pieces/Pawn.cs
@@ -6,10 +6,15 @@ namespace ChessSharp.Pieces
 {
     public class Pawn : Piece
     {
-        private (int normalMove, int firstMove) validDistances = (1, 4);
+        /// <summary>Used to determing if pawn can be captured enpassant </summary>
         public bool CanBeCapturedEnPassant { get; set; } = false;
+        /// <summary>Util props for moving validation </summary>
         private int startingRank { get; }
-
+        private (int normalMove, int firstMove) validDistances = (1, 4);
+        /// <summary>
+        /// Just a constructor
+        /// </summary>
+        /// <param name="isWhite">boolean to determine if the piece is white or black</param>
         public Pawn(bool isWhite) : base(isWhite)
         {
             pieceChar = 'P';
@@ -19,7 +24,15 @@ namespace ChessSharp.Pieces
             else
                 startingRank = 6;
         }
-
+        /// <summary>
+        /// The Pawn's Implementation of <see cref="Piece.CanMove(Grid, Move)"/>
+        /// Uses piece implementation of this func <see cref="Piece"/>
+        /// <see cref="Grid.Distance(Tile, Tile)"/>
+        /// <see cref="Grid.IsLegalMove(Move, bool)"/>
+        /// </summary>
+        /// <param name="board">The board <see cref="Grid"/></param>
+        /// <param name="move">The move to check <see cref="Move"/></param>
+        /// <returns>True if the move is valid</returns>
         public override bool CanMove(Grid board, Move move)
         {
             if(base.CanMove(board, move))
@@ -151,7 +164,14 @@ namespace ChessSharp.Pieces
             }
             return false;
         }
-
+        /// <summary>
+        /// Pawn's implementation of the abstract <see cref="Piece.IsAttackingTile(Grid, Tile, Tile)"/> method
+        /// Used to check if Pawn is attacking a given tile
+        /// </summary>
+        /// <param name="board">The board <see cref="Grid"/></param>
+        /// <param name="piecePos">The pawn's tile <see cref="Tile"/></param>
+        /// <param name="destionation">The tile to check <see cref="Tile"/></param>
+        /// <returns>True if the condition is met, false if not</returns>
         public override bool IsAttackingTile(Grid board, Tile piecePos, Tile destionation)
         {
             if (piecePos.piece.IsWhite)
