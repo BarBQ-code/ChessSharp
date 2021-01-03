7ca2c6c4866835ef79c1a63fbd37842e9dc7f86a
Michael Ben Yosef <mishico5@gmail.com>
2021-01-03 17:29:25 +0200

    Doucement Knight

--- Pieces/Knight.cs
+++ Pieces/Knight.cs
@@ -5,12 +5,20 @@ namespace ChessSharp.Pieces
 {
     public class Knight : Piece
     {
+        /// <summary>Util prop for checking valid distance</summary>
         private const int validDistance = 5;
         public Knight(bool isWhite) : base(isWhite)
         {
             pieceChar = 'N';
         }
-
+        /// <summary>
+        /// Knight's implementation of <see cref="Piece.CanMove(Grid, Move)"/>
+        /// Calls the base canmove method <see cref="Piece"/>
+        /// Also uses <see cref="IsAttackingTile(Grid, Tile, Tile)"/> and <see cref="Grid.IsLegalMove(Move, bool)"/>
+        /// </summary>
+        /// <param name="board">The board <see cref="Grid"/></param>
+        /// <param name="move">The move to check <see cref="Move"/></param>
+        /// <returns>True if the move is valid, false if not</returns>
         public override bool CanMove(Grid board, Move move)
         {
             if(base.CanMove(board, move))
@@ -28,7 +36,14 @@ namespace ChessSharp.Pieces
             }
             return false;
         }
-
+        /// <summary>
+        /// Knight's implementation of the abstract <see cref="Piece.IsAttackingTile(Grid, Tile, Tile)"/> method
+        /// Uses the <see cref="Grid.Distance(Tile, Tile)"/> for move validation
+        /// </summary>
+        /// <param name="board">The board <see cref="Grid"/></param>
+        /// <param name="piecePos">The knight's position <see cref="Tile"/></param>
+        /// <param name="destionation">The dest tile <see cref="Tile"/></param>
+        /// <returns>True if the condition is met, false if not</returns>
         public override bool IsAttackingTile(Grid board, Tile piecePos, Tile destionation)
         { 
             if (Grid.Distance(piecePos, destionation) == validDistance)
