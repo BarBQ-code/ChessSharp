a5affcc722a99094244a0e1a257c2141db221b94
Michael Ben Yosef <mishico5@gmail.com>
2021-01-11 12:47:47 +0200

    Init commit

---
+++ ChessSharp.Tests/MoveTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChessSharp;
+using Xunit;
+
+namespace ChessSharp.Tests
+{
+    public class MoveTests
+    {
+
+    }
+}
