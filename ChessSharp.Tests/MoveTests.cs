7237c06901c934ef2c9d828717e024c5095e59aa
Michael Ben Yosef <mishico5@gmail.com>
2021-01-11 13:27:39 +0200

    Add Exception tests for Move.FromUCI

--- ChessSharp.Tests/MoveTests.cs
+++ ChessSharp.Tests/MoveTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using ChessSharp;
+using ChessSharp.Exceptions;
 using ChessSharp.Pieces;
 using Xunit;
 
@@ -40,6 +41,45 @@ namespace ChessSharp.Tests
             Move actual = Move.FromUCI(board, "h7h8", new Queen(true));
 
             Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMoveFromUCIExceptions()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Grid board = new Grid();
+                Move.FromUCI(board, "123"); //uci string must be 4 chars
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid();
+                Move.FromUCI(board, "e4e2"); //Source tile has no piece
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid();
+                Move.FromUCI(board, "e2d2"); //Source and dest tiles have pieces of the same team
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid("4k3/7R/8/8/8/8/8/4K3 w - - 0 1");
+                Move.FromUCI(board, "h7h8", new Queen(true)); //In a promotion move source tile must contain pawn
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid("4k3/8/7P/8/8/8/8/4K3 w - - 0 1");
+                Move.FromUCI(board, "h6h7", new Queen(true)); //In a promotion move dest tile must be the last rank
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid("4k3/8/8/8/8/p7/8/4K3 b - - 0 1");
+                Move.FromUCI(board, "a3a2", new Queen(false)); //In a promotion move dest tile must be the first rank
+            });
+            Assert.Throws<InvalidMoveException>(() =>
+            {
+                Grid board = new Grid("4k3/7P/8/8/8/8/8/4K3 w - - 0 1");
+                Move.FromUCI(board, "h7h8", new Pawn(true)); //Can't promote to a pawn
+            });
         }
     }
 }
