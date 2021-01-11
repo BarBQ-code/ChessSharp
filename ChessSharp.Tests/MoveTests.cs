e184b7a94a7075f13612a43f8f27ce1175b5603c
Michael Ben Yosef <mishico5@gmail.com>
2021-01-11 13:10:00 +0200

    Add Two simple tests

--- ChessSharp.Tests/MoveTests.cs
+++ ChessSharp.Tests/MoveTests.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using ChessSharp;
+using ChessSharp.Pieces;
 using Xunit;
 
 namespace ChessSharp.Tests
 {
     public class MoveTests
     {
+        [Fact]
+        public void TestMoveFromUCIE4()
+        {
+            Grid board = new Grid();
+            Move expected = new Move(new Tile(new Pawn(true), 4, 1), new Tile(null, 4, 3), board.CurrentPlayer);
 
+            Move actual = Move.FromUCI(board, "e2e4");
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMoveFromUCIE5()
+        {
+            Grid board = new Grid();
+            board.MakeMove(Move.FromUCI(board, "e2e4"));
+
+            Move expected = new Move(new Tile(new Pawn(false), 4, 6), new Tile(null, 4, 4), board.CurrentPlayer);
+
+            Move actual = Move.FromUCI(board, "e7e5");
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
