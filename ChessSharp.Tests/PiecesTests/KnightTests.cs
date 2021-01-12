4b421bcb57b1322b22351b46c526a4ea4d4a2bd4
Michael Ben Yosef <mishico5@gmail.com>
2021-01-12 14:07:55 +0200

    Init commit

---
+++ ChessSharp.Tests/PiecesTests/KnightTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ChessSharp;
+
+namespace ChessSharp.Tests.PiecesTests
+{
+    public class KnightTests
+    {
+
+    }
+}
