16b376e6ef549bbc963a3a470fa500c737c75a16
Michael Ben Yosef <mishico5@gmail.com>
2021-01-14 13:49:29 +0200

    Add WhiteKingNormalMoves test

--- ChessSharp.Tests/PiecesTests/KingTests.cs
+++ ChessSharp.Tests/PiecesTests/KingTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -7,5 +8,58 @@ namespace ChessSharp.Tests.PiecesTests
 {
     public class KingTests
     {
+        [Fact]
+        public void TestWhiteKingNormalMoves()
+        {
+            //In init pos there are no moves for the king
+            Grid board = new Grid();
+            Tile kingPos = board.GetTile(4, 0);
+
+            Assert.True(kingPos.Piece.GetAllMoves(board, kingPos).Count == 0);
+
+            board = new Grid("4k3/8/8/8/8/8/8/4K3 w - - 0 1");
+            kingPos = board.GetTile(4, 0);
+
+            //check normal moves
+            List<Move> kingLegalMoves = kingPos.Piece.GetAllMoves(board, kingPos);
+            List<Move> moves = new List<Move>
+            {
+                Move.FromUCI(board, "e1f1"),
+                Move.FromUCI(board, "e1d1"),
+                Move.FromUCI(board, "e1d2"),
+                Move.FromUCI(board, "e1e2"),
+                Move.FromUCI(board, "e1f2"),
+            };
+
+            Assert.True(kingLegalMoves.All(moves.Contains) && kingLegalMoves.Count == moves.Count);
+
+            //King is cut off, can move only up
+            board = new Grid("3rkr2/8/8/8/8/8/8/4K3 w - - 0 1");
+            kingPos = board.GetTile(4, 0);
+            kingLegalMoves = kingPos.Piece.GetAllMoves(board, kingPos);
+
+            Assert.True(kingLegalMoves.Count == 1);
+            Assert.True(kingPos.Piece.CanMove(board, Move.FromUCI(board, "e1e2")));
+        }
+        [Fact]
+        public void TestWhiteKingCastles()
+        {
+            Grid board = new Grid("4k3/8/8/8/8/8/8/R3K2R w KQ - 0 1");
+            Tile kingPos = board.GetTile(4, 0);
+
+            List<Move> kingLegalMoves = kingPos.Piece.GetAllMoves(board, kingPos);
+            List<Move> moves = new List<Move>
+            {
+                Move.FromUCI(board, "e1f1"),
+                Move.FromUCI(board, "e1d1"),
+                Move.FromUCI(board, "e1d2"),
+                Move.FromUCI(board, "e1e2"),
+                Move.FromUCI(board, "e1f2"),
+                Move.FromUCI(board, "e1g1"),
+                Move.FromUCI(board, "e1c1")
+            };
+
+            Assert.True(kingLegalMoves.All(moves.Contains) && kingLegalMoves.Count == moves.Count);
+        }
     }
 }
