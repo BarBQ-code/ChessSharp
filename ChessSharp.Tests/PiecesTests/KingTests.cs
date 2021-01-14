a6d1dbe7113af899f3c472e8c888cb8913993cab
Michael Ben Yosef <mishico5@gmail.com>
2021-01-14 14:02:23 +0200

    Add BlackKingNormalMoves Test

--- ChessSharp.Tests/PiecesTests/KingTests.cs
+++ ChessSharp.Tests/PiecesTests/KingTests.cs
@@ -68,5 +68,38 @@ namespace ChessSharp.Tests.PiecesTests
             Assert.False(kingPos.Piece.CanMove(board, Move.FromUCI(board, "e1g1")));
             Assert.False(kingPos.Piece.CanMove(board, Move.FromUCI(board, "e1c1")));
         }
+        [Fact]
+        public void TestBlackKingCanMove()
+        {
+            //In init pos there are no moves for the king
+            Grid board = new Grid();
+            Tile kingPos = board.GetTile(4, 7);
+
+            Assert.True(kingPos.Piece.GetAllMoves(board, kingPos).Count == 0);
+
+            board = new Grid("4k3/8/8/8/8/8/8/4K3 b - - 0 1");
+            kingPos = board.GetTile(4, 7);
+
+            //check normal moves
+            List<Move> kingLegalMoves = kingPos.Piece.GetAllMoves(board, kingPos);
+            List<Move> moves = new List<Move>
+            {
+                Move.FromUCI(board, "e8f8"),
+                Move.FromUCI(board, "e8d8"),
+                Move.FromUCI(board, "e8d7"),
+                Move.FromUCI(board, "e8e7"),
+                Move.FromUCI(board, "e8f7"),
+            };
+
+            Assert.True(kingLegalMoves.All(moves.Contains) && kingLegalMoves.Count == moves.Count);
+
+            //King is cut off, can move only up
+            board = new Grid("4k3/8/8/8/8/8/8/3RKR2 w - - 0 1");
+            kingPos = board.GetTile(4, 7);
+            kingLegalMoves = kingPos.Piece.GetAllMoves(board, kingPos);
+
+            Assert.True(kingLegalMoves.Count == 1);
+            Assert.True(kingPos.Piece.CanMove(board, Move.FromUCI(board, "e8e7")));
+        }
     }
 }
